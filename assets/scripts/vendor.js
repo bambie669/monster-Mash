478ce1255baa8ecd721a04d255e9bfb9605a3b8f
bambie669 <raduroman94@gmail.com>
2020-09-15 17:35:54 +0900

    vendor scripts updated

--- assets/scripts/vendor.js
+++ assets/scripts/vendor.js
@@ -5,29 +5,40 @@ const bonusLifeEl = document.getElementById('bonus-life');
 const attackBtn = document.getElementById('attack-btn');
 const strongAttackBtn = document.getElementById('strong-attack-btn');
 const healBtn = document.getElementById('heal-btn');
-// const logBtn = document.getElementById('log-btn');
+const logBtn = document.getElementById('log-btn');
 
-function monsterDamage(damage) {
+function adjustHealthBars(maxLife) {
+  monsterHealthBar.max = maxLife;
+  monsterHealthBar.value = maxLife;
+  playerHealthBar.max = maxLife;
+  playerHealthBar.value = maxLife;
+}
+
+function dealMonsterDamage(damage) {
   const dealtDamage = Math.random() * damage;
   monsterHealthBar.value = +monsterHealthBar.value - dealtDamage;
   return dealtDamage;
 }
 
-function playerDamage(damage) {
+function dealPlayerDamage(damage) {
   const dealtDamage = Math.random() * damage;
   playerHealthBar.value = +playerHealthBar.value - dealtDamage;
   return dealtDamage;
 }
 
-function healPlayer(healValue) {
+function increasePlayerHealth(healValue) {
   playerHealthBar.value = +playerHealthBar.value + healValue;
 }
 
-function playerHealth(health) {
-  playerHealthBar.value = health;
-}
-
-function ffGame(value) {
+function resetGame(value) {
   playerHealthBar.value = value;
   monsterHealthBar.value = value;
+}
+
+function removeBonusLife() {
+  bonusLifeEl.parentNode.removeChild(bonusLifeEl);
+}
+
+function setPlayerHealth(health) {
+  playerHealthBar.value = health;
 }
