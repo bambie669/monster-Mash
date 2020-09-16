441138557c9f58985f066ae6a22c3c56684eb5f2
bambie669 <raduroman94@gmail.com>
2020-09-17 00:26:59 +0900

    attack function added

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,3 +1,16 @@
-// const attackValue = 10; 
-// let chosenMaxLife = 100;
+const ATTACK_VALUE = 10;
 
+let chosenMaxLife = 100;
+let currentMonsterHealth = chosenMaxLife;
+let currentPlayerHealth = chosenMaxLife
+
+adjustHealthBars(chosenMaxLife)
+
+function attackMonster() {
+    const damage = dealMonsterDamage(ATTACK_VALUE);
+    currentMonsterHealth -= damage;
+
+
+}
+
+attackBtn.addEventListener('click', attackMonster);
