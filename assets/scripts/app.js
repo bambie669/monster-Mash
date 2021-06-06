a6854e7b8e7bcd64a8c755515f7cdbb76558f5b7
bambie669 <raduroman94@gmail.com>
2021-06-06 17:34:42 +0300

    const added for attack/strong attack

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,7 +3,17 @@ const STRONG_ATTACK_VALUE = 17;
 const MONSTER_ATTACK_VALUE = 14;
 const HEAL_VALUE = 20;
 
-let chosenMaxLife = 100;
+const ATTACK_STANCE = 'ATTACK'; // Stance = 0
+const STRONG_ATTACK_STANCE = 'STRONG_ATTACK'; // Stance = 1
+
+const enteredValue = prompt('maximum life for player and monster', '100');
+
+let chosenMaxLife = parseInt(enteredValue); 
+
+if (isNaN(chosenMaxLife) || chosenMaxLife <= 0 ) {
+    chosenMaxLife = 100;
+}
+
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
 let hasBonusLife = true;
@@ -41,18 +51,15 @@ function endRound() {
   }
 }
 
-if (currentMonsterHealth <= 0 || currentPlayerHealth <=0 ){
-    reset();
+if (currentMonsterHealth <= 0 || currentPlayerHealth <= 0) {
+  reset();
 }
-  
-
-
 
 function attackMonster(stance) {
   let maxDamage;
-  if (stance === 'ATTACK') {
+  if (stance === 'ATTACK_STANCE') {
     maxDamage = ATTACK_VALUE;
-  } else if (stance === 'STRONG_ATTACK') {
+  } else if (stance === 'STRONG_ATTACK_STANCE') {
     maxDamage = STRONG_ATTACK_VALUE;
   }
   const damage = dealMonsterDamage(maxDamage);
@@ -61,11 +68,11 @@ function attackMonster(stance) {
 }
 
 function attackHandler() {
-  attackMonster('ATTACK');
+  attackMonster('ATTACK_STANCE');
 }
 
 function strongAttackHandler() {
-  attackMonster('STRONG_ATTACK');
+  attackMonster('STRONG_ATTACK_STANCE');
 }
 
 function healPlayerHandler() {
